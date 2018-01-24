a3d40379eb0e4c307f992d49995cf1e197167856
jacksyang <513980580@qq.com>
2018-01-24 18:11:14 +0800

    123

--- 2/2.js
+++ 2/2.js
@@ -2,3 +2,18 @@
 // git fetch origin master 先拉到本地
 // git log -p master..origin/master
 // git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
+// git merge origin/master 然后合并到本地
